9b228e7a54e3a509b1799045eb4323675eb18277
MUaRka <lexminer2014@gmail.com>
2024-05-20 15:17:35 +0300

    Add project files.

---
+++ KKLauncher.Bot/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace KKLauncher.Bot
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
