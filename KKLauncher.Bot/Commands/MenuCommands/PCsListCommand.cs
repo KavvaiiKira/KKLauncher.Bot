5aea2ab542529783f082c3e7d2079512e1791553
MUaRka <lexminer2014@gmail.com>
2024-05-21 17:40:43 +0300

    Db context added

--- KKLauncher.Bot/Commands/MenuCommands/PCsListCommand.cs
+++ KKLauncher.Bot/Commands/MenuCommands/PCsListCommand.cs
@@ -1,4 +1,7 @@
-﻿using Telegram.Bot;
+﻿using KKLauncher.Bot.EF;
+using KKLauncher.DB.Entities;
+using Microsoft.EntityFrameworkCore;
+using Telegram.Bot;
 
 namespace KKLauncher.Bot.Commands.MenuCommands
 {
@@ -6,7 +9,19 @@ namespace KKLauncher.Bot.Commands.MenuCommands
     {
         public async Task ExecuteAsync(ITelegramBotClient kkBot, long chatId)
         {
-            return;
+            List<PCEntity>? pcs = null;
+
+            using (KKBotDbContext db = new KKBotDbContext())
+            {
+                pcs = db.PCs.AsNoTracking().ToList();
+            }
+
+            if (pcs == null || !pcs.Any())
+            {
+                await kkBot.SendTextMessageAsync(chatId: chatId, text: "No PCs added.");
+
+                return;
+            }
         }
     }
 }
--- KKLauncher.Bot/EF/KKBotDbContext.cs
+++ KKLauncher.Bot/EF/KKBotDbContext.cs
@@ -1,6 +1,49 @@
-﻿namespace KKLauncher.Bot.EF
+﻿using KKLauncher.DB.Entities;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+
+namespace KKLauncher.Bot.EF
 {
-    public class KKBotDbContext
+    public class KKBotDbContext : DbContext
     {
+        public KKBotDbContext()
+        {
+        }
+
+        public virtual DbSet<PCEntity> PCs { get; set; }
+
+        public virtual DbSet<AppEntity> Applications { get; set; }
+
+        public virtual DbSet<CollectionEntity> Collections { get; set; }
+
+        public virtual DbSet<AppCollectionEntity> AppCollection { get; set; }
+
+        public virtual DbSet<WhitelistEntity> Whitelists { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = JObject.Parse(File.ReadAllText(Path.Combine("config", "kk-bot.json")));
+
+            optionsBuilder.UseNpgsql(config["ConnectionString"].ToString());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<AppEntity>()
+                .HasMany(ac => ac.Collections)
+                .WithMany(ac => ac.Apps)
+                .UsingEntity<AppCollectionEntity>(
+                    c => c.HasOne<CollectionEntity>().WithMany().HasForeignKey(ac => ac.CollectionId),
+                    a => a.HasOne<AppEntity>().WithMany().HasForeignKey(ac => ac.AppId));
+
+            modelBuilder
+                .Entity<CollectionEntity>()
+                .HasMany(ac => ac.Apps)
+                .WithMany(ac => ac.Collections)
+                .UsingEntity<AppCollectionEntity>(
+                    a => a.HasOne<AppEntity>().WithMany().HasForeignKey(ac => ac.AppId),
+                    c => c.HasOne<CollectionEntity>().WithMany().HasForeignKey(ac => ac.CollectionId));
+        }
     }
 }
